3f944720182d14ace87ba522cb4d4752a333e48f
Marcelo Prado <Devfrontend2028study@gmail.com>
2025-01-10 15:47:24 -0300

    Atualizar o Advice.jsx

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -3,21 +3,21 @@
 export default function Advice({advice, id, ImageMobile, ImageDesk, dice, hundleUpdateQuote}) {
   
     return (
-        <article id="Ad--container">
-           <section id="Advice-content">
+        <div id="Ad--container">
+           <div id="Advice-content">
            <h1 id="Advice-num">Advice #{id}</h1>
             <p id="Advice-quote">“{advice}”</p>
            <div id="divider-line">
            <img src={ImageMobile} alt="" id="mobile-img"/>
            <img src={ImageDesk} alt="" id="desktop-img"/>
            </div>
-           </section>
+           </div>
            <div id="dice--container">
            <button id="dice-btn" onClick={() =>  hundleUpdateQuote()}>
            <img src={dice} alt="" id="dice"/>
            </button>
            </div>
-           </article>
+           </div>
     )
 
 } 
